90007df096c66c2c228cb29a0ac5868d361799c0
shuheikomatsuki <shuheijy@gmail.com>
2025-05-27 21:43:19 +0900

    Add keep_alive server for Render

--- bot.py
+++ bot.py
@@ -1,3 +1,9 @@
+# bot.py の先頭あたりに以下を追加
+from keep_alive import keep_alive
+
+keep_alive()  # ダミーWebサーバーを起動する
+
+# 以下はそのまま
 import discord
 import os
 
@@ -10,8 +16,8 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content == 'ping':
-        await message.channel.send('pong!')
+    if message.content == "ping":
+        await message.channel.send("pong!")
 
-TOKEN = os.getenv('DISCORD_TOKEN')
+TOKEN = os.getenv("DISCORD_TOKEN")
 client.run(TOKEN)
